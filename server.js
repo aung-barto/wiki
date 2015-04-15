2c0f9b955cbddbcdfc2ea7a227dc0a576d927e75
Aungwara Barteaux <aungwaran@gmail.com>
2015-04-15 13:00:31 -0400

    show page

--- server.js
+++ server.js
@@ -34,5 +34,33 @@ app.get("/wiki", function(req,res){
 	});
 });
 
+//show individual article
+app.get("/article/:id", function(req,res){
+	var artID = parseInt(req.params.id);
+	db.get("SELECT * FROM articles WHERE id = " + artID, function(err,data){
+		if(err){
+			console.log(err);
+		}else{
+			var thisArt = data;
+			//getting author of the article
+			db.get("SELECT users.name FROM users INNER JOIN articles ON articles.author_id = users.id WHERE articles.id = " + artID, function(err,data){
+				if(err){
+					console.log(err);
+				}else{
+					var one_author = data;
+					db.get("SELECT users.name FROM co_authors INNER JOIN users ON co_authors.user_id = users.id WHERE co_authors.article_id = " + artID, function(err,data){
+						if(err){
+							console.log(err);
+						}else{
+							var allCoAut = data;
+						}
+						res.render("show.ejs", {articles: thisArt, users: one_author, co_authors: allCoAut});
+					});
+				}
+			});
+		}
+	});
+});
+
 app.listen("3000");
 console.log("Server listening to port 3000");
